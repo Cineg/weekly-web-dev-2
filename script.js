862827738f8f2f2a8904096f0add2785b108ccec
cineg <cineg7@gmail.com>
2018-07-10 13:07:38 +0200

    small visual changes

---
+++ script.js
@@ -0,0 +1,33 @@
+/* NAV STYLE */
+const nav = document.getElementById('navigation');
+
+window.onscroll = function(){
+    if(window.pageYOffset > 100){
+        nav.classList.add('scrolled');
+    }
+    else{
+        nav.classList.remove('scrolled');
+    }
+}
+
+
+
+/* INPUTS STYLE */
+//get input containers
+let inputContainers = document.getElementsByClassName('input-field');
+
+//for each not working (dunno why) 
+for (let i= 0; i < inputContainers.length; i++) {
+    //get active class
+    const inputActive = inputContainers[i].querySelector('.active');
+    //get input
+    const input = inputContainers[i].querySelector('.input');
+    //if input is active, div active is visible
+    input.addEventListener("focusin", () => {
+        inputActive.classList.add('visible');
+    });
+    //otherwise is not visible
+    input.addEventListener('focusout', () => {
+        inputActive.classList.remove('visible');
+    })
+}
